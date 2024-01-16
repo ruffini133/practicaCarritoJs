d9c8df2ac4c3730a9f614579ba5ec7572ba8bf9e
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-01-16 05:36:14 -0600

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,83 @@
+// Variables
+const carrito = document.querySelector("#carrito");
+
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+
+const listaCursos = document.querySelector("#lista-cursos");
+
+let articulosCarrito = [];
+
+cargarEventListeners();
+function cargarEventListeners() {
+	listaCursos.addEventListener("click", agregarCurso);
+}
+
+// Funciones
+function agregarCurso(e) {
+	e.preventDefault();
+
+	if (e.target.classList.contains("agregar-carrito")) {
+		const cursoSeleccionado = e.target.parentElement.parentElement;
+
+		leerDatosCurso(cursoSeleccionado);
+	}
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
+function leerDatosCurso(curso) {
+	// console.log(curso);
+
+	// Crear un objeto con el contenido del curso actual
+	const infoCurso = {
+		imagen: curso.querySelector("img").src,
+		titulo: curso.querySelector("h4").textContent,
+		precio: curso.querySelector(".precio span").textContent,
+		id: curso.querySelector("a").getAttribute("data-id"),
+		cantidad: 1,
+	};
+
+	// Agrega elementos al arreglo de carrito
+	articulosCarrito = [...articulosCarrito, infoCurso];
+
+	console.log(articulosCarrito);
+
+	carritoHTML();
+}
+
+// Muestra el carrito de compras en el HTML
+function carritoHTML() {
+	// Limpiar el HTML
+	limpiarHTML();
+
+	// Recorre el carrito y genera el HTML
+	articulosCarrito.forEach((curso) => {
+		const row = document.createElement("tr");
+		row.innerHTML = `
+			<td>
+				<img src="${curso.imagen}" width="100"
+			</td>
+
+			<td>
+				${curso.titulo}
+			</td>
+
+			<td>
+				${curso.precio}
+			</td>
+
+			<td>
+				${curso.cantidad}
+			</td>
+		`;
+
+		// Agrega el HTML del carrito en el tbody
+		contenedorCarrito.appendChild(row);
+	});
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+	contenedorCarrito.innerHTML = "";
+}
